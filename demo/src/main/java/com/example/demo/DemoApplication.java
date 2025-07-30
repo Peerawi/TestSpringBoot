1cecc1855d9e399cec58d4644438da938ae116c6
MyArmy <64011212088@msu.ac.th>
2025-07-30 23:07:23 +0700

    PostTry

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,9 @@ public class DemoApplication {
       return String.format("Hello %s", name);
     }
     
-
+    @PostMapping("/hello")
+    public DemoResponse hello(@RequestBody DemoName request) {
+        String name = request.getName();
+        return new DemoResponse("Hello, " + name);
+    }
 }
